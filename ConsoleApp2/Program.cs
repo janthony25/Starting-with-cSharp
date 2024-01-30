3efdf972f7c3752feb28dfe529df974ae7e2e0a6
janthony25 <junanthony.business@gmail.com>
2024-01-30 19:52:22 +1300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+       static void Main(string[] args)
+        {
+            string uname = "junanthony25";
+            char symbol = '@';
+
+            string username = symbol + uname;
+
+            Console.WriteLine($"Your username is {username}");
+        }
+    }
+}
